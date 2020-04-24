b780a0e9b2d96e42df6f8edc4163e5cf5bf8b429
zhangqiao <1226937462@qq.com>
2020-04-24 17:52:04 +0800

    config icon

---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="em-header">
+    <div class="em-header-button is-left" @click="goBack"> <i class="fa fa-calendar-alt"></i></div>
+    <h3 class="em-header-title">{{title}}</h3>
+    <div class="em-header-button is-right"><i class="fa fa-calendar-alt"></i></div>
+  </div>
+</template>
+<script>
+    export default {
+      name:'Header',
+      props:{
+        title:String
+      },
+      methods:{
+        goBack(){
+          this.$router.go(-1)
+        }
+      }
+    }
+</script>
+<style scoped lang="less">
+    .em-header{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        height: 3.4rem;
+        padding: 0 1rem;
+        background: #2088fd;
+        color: #fff;
+        .em-header-button{
+            .is-left{
+                text-align: left;
+            }
+            .is-right{
+                text-align: right;
+            }
+        }
+        .em-header-title{
+            font-weight: normal;
+        }
+    }
+</style>
