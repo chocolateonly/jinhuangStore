94bfd080435993c346427c94c70c2726270f6ed7
zhangqiao <1226937462@qq.com>
2020-05-01 16:18:56 +0800

    config hot loader

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+/*  publicPath: process.env.NODE_ENV === 'production'
+    ? '/production/' //项目部署到服务器的路径配置
+    : '/'*/
+  //publicPath:'./',
+  //outputDir:'test',
+  //indexPath:"index.html",
+  devServer : {
+    hot:true,//hot loader------start
+    open : true,
+    port : 8080,
+    host : "127.0.0.1"//hot loader----end
+  }
+}
